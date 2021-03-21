aa3390d3d51790c3afc13b3ebdc07bb6d14b08da
s-ike <shunt_i@yahoo.co.jp>
2021-03-21 15:13:23 +0900

    add #39 unset function

--- srcs/env_utils.c
+++ srcs/env_utils.c
@@ -20,3 +20,33 @@ char
 	}
 	return (NULL);
 }
+
+int
+	ft_unsetenv(const char *name)
+{
+	t_list	*envptr;
+	t_list	*prev;
+	size_t	len;
+
+	if (!g_env || !name || !*name)
+		return (ENVUTIL_ERROR);
+	envptr = g_env;
+	prev = NULL;
+	len = ft_strlen(name);
+	while (envptr)
+	{
+		if (!ft_strncmp(name, envptr->content, len)
+		&& ((char*)envptr->content)[len] == '=')
+		{
+			if (prev)
+				prev->next = envptr->next;
+			else
+				g_env = envptr->next;
+			ft_lstdelone(envptr, free);
+			return (ENVUTIL_SUCCESS);
+		}
+		prev = envptr;
+		envptr = envptr->next;
+	}
+	return (ENVUTIL_ERROR);
+}
