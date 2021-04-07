abb6230a642e8d8ea119e56994a11e92f552df5e
s-ike <shunt_i@yahoo.co.jp>
2021-04-08 00:00:42 +0900

    update #50 to update env values when cd function is executed

--- cdtest.sh
+++ cdtest.sh
@@ -1,180 +1,262 @@
 #!/bin/bash
 
 cd libft
-make
+make bonus
 cd ..
-gcc -Wall -Wextra -Werror -I./includes -I./libft srcs/cd.c srcs/pwd.c srcs/command_utils.c srcs/env_utils.c -Llibft -lft -D CDTEST -o cd.out
+gcc -g -Wall -Wextra -Werror -I./includes -I./libft srcs/cd.c srcs/pwd.c srcs/exit.c srcs/env.c srcs/unset.c srcs/export.c srcs/command_utils.c srcs/env_utils.c -Llibft -lft -D CDTEST -o cd.out
 
 YELLOW=$(printf '\033[33m')
+CYAN=$(printf '\033[36m')
 RESET=$(printf '\033[0m')
 
 WORKDIR=`pwd`
+echo ""
 
-printf "${YELLOW}%s${RESET}\n" "./cd.out"
-./cd.out
-echo
-
-printf "${YELLOW}%s${RESET}\n" "./cd.out cd"
+# 引数なし
+echo "※minishellではパスを引数に取らない場合invalid"
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
+printf "${YELLOW}%s${RESET}\n" "[mini] cd"
 ./cd.out cd
+echo $?
+printf "${YELLOW}%s${RESET}\n" "[bash] cd"
+echo "cd" | bash
+echo $?
+echo "pwd" | bash
 echo
 
-TEST3="~"
+# 引数特殊 ~ (シェルスクリプトによりminishellのテストも ~ が変数展開されている)
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "[mini] cd ~"
 ./cd.out cd ~
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd ~"
-pwd
 cd ~
+echo $?
 pwd
+printf "PWD: ${PWD}\n"
+printf "OLDPWD: ${OLDPWD}\n"
 cd $WORKDIR
 echo
 
+# cd "~"
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "[mini] cd \"~\""
 ./cd.out cd "~"
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd \"~\""
-pwd
 cd "~"
-pwd
+echo $?
 cd $WORKDIR
 echo
 
+# cd ""
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "[mini] cd \"\""
 ./cd.out cd ""
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd \"\""
-pwd
 cd ""
+echo $?
 pwd
+printf "PWD: ${PWD}\n"
+printf "OLDPWD: ${OLDPWD}\n"
 cd $WORKDIR
 echo
 
+# cd ''
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "[mini] cd ''"
 ./cd.out cd ''
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd ''"
-pwd
 cd ''
+echo $?
 pwd
+printf "PWD: ${PWD}\n"
+printf "OLDPWD: ${OLDPWD}\n"
 cd $WORKDIR
 echo
 
+# cd $HOME
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "[mini] cd \$HOME"
 ./cd.out cd $HOME
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd \$HOME"
-pwd
 cd $HOME
+echo $?
 pwd
+printf "PWD: ${PWD}\n"
+printf "OLDPWD: ${OLDPWD}\n"
 cd $WORKDIR
 echo
 
+# cd hoge (No such file or directory)
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "[mini] cd hoge"
 ./cd.out cd hoge
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd hoge"
-pwd
 cd hoge
-pwd
-echo
+echo $?
 cd $WORKDIR
+echo
 
+# cd cdtest
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "mkdir cdtest"
 mkdir cdtest
 printf "${YELLOW}%s${RESET}\n" "[mini] cd cdtest"
 ./cd.out cd cdtest
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd cdtest"
-pwd
 cd cdtest
+echo $?
 pwd
-echo
+printf "PWD: ${PWD}\n"
+printf "OLDPWD: ${OLDPWD}\n"
 cd $WORKDIR
+rm -rf cdtest
+echo
 
+# cd "cdtest"
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
+printf "${YELLOW}%s${RESET}\n" "mkdir cdtest"
+mkdir cdtest
 printf "${YELLOW}%s${RESET}\n" "[mini] cd \"cdtest\""
 ./cd.out cd "cdtest"
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd \"cdtest\""
-pwd
 cd "cdtest"
+echo $?
 pwd
-echo
+printf "PWD: ${PWD}\n"
+printf "OLDPWD: ${OLDPWD}\n"
 cd $WORKDIR
+rm -rf cdtest
+echo
 
+# cd "cdtest "
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
+printf "${YELLOW}%s${RESET}\n" "mkdir cdtest"
+mkdir cdtest
 printf "${YELLOW}%s${RESET}\n" "[mini] cd \"cdtest \""
 ./cd.out cd "cdtest "
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd \"cdtest \""
-pwd
 cd "cdtest "
-pwd
-echo
+echo $?
 cd $WORKDIR
+rm -rf cdtest
+echo
 
+# cd "cdtest"
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
+printf "${YELLOW}%s${RESET}\n" "mkdir cdtest"
+mkdir cdtest
+printf "${YELLOW}%s${RESET}\n" "[mini] cd cdtest hoge"
+./cd.out cd cdtest hoge
+echo $?
+printf "${YELLOW}%s${RESET}\n" "[bash] cd cdtest hoge"
+cd cdtest hoge
+echo $?
+pwd
+printf "PWD: ${PWD}\n"
+printf "OLDPWD: ${OLDPWD}\n"
+cd $WORKDIR
+rm -rf cdtest
+echo
+
+# cd "hoge world"
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "[mini] cd hoge world"
 ./cd.out cd hoge world
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd hoge world"
-pwd
 cd hoge world
-pwd
-echo
+echo $?
 cd $WORKDIR
-
-printf "${YELLOW}%s${RESET}\n" "[mini] cd hoge world"
-./cd.out cd hoge world
-printf "${YELLOW}%s${RESET}\n" "[bash] cd hoge world"
-pwd
-cd hoge world
-pwd
 echo
-cd $WORKDIR
 
+# cd -a
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "[mini] cd -a"
 ./cd.out cd -a
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd -a"
-pwd
 cd -a
-pwd
-echo
+echo $?
 cd $WORKDIR
+echo
 
+# cd ./-a (mkdir ./-a)
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "mkdir ./-a"
 mkdir ./-a
 printf "${YELLOW}%s${RESET}\n" "[mini] cd ./-a"
 ./cd.out cd ./-a
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd ./-a"
-pwd
 cd ./-a
+echo $?
 pwd
-echo
+printf "PWD: ${PWD}\n"
+printf "OLDPWD: ${OLDPWD}\n"
 cd $WORKDIR
 rm -rf ./-a
+echo
 
+# cd -a cdtest
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
+printf "${YELLOW}%s${RESET}\n" "mkdir cdtest"
+mkdir cdtest
 printf "${YELLOW}%s${RESET}\n" "[mini] cd -a cdtest"
 ./cd.out cd -a cdtest
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd -a cdtest"
-pwd
 cd -a cdtest
-pwd
-echo
+echo $?
 cd $WORKDIR
-
-printf "${YELLOW}%s${RESET}\n" "[mini] cd --version cdtest"
-./cd.out cd --version cdtest
-printf "${YELLOW}%s${RESET}\n" "[bash] cd --version cdtest"
-pwd
-cd --version cdtest
-pwd
+rm -rf cdtest
 echo
-cd $WORKDIR
 
+# cd --version
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
+printf "${YELLOW}%s${RESET}\n" "[mini] cd --version"
+./cd.out cd --version
+echo $?
+printf "${YELLOW}%s${RESET}\n" "[bash] cd --version"
+cd --version
+echo $?
+cd $WORKDIR
+echo
+
+# cd --
+echo "※minishellではパスを引数に取らない場合invalid"
+printf "${CYAN}%s${RESET}\n" "pwd:
+${WORKDIR}"
 printf "${YELLOW}%s${RESET}\n" "[mini] cd --"
 ./cd.out cd --
+echo $?
 printf "${YELLOW}%s${RESET}\n" "[bash] cd --"
-pwd
 cd --
+echo $?
 pwd
-echo
 cd $WORKDIR
-
-printf "${YELLOW}%s${RESET}\n" "[mini] cd -- cdtest"
-./cd.out cd -- cdtest
-printf "${YELLOW}%s${RESET}\n" "[bash] cd -- cdtest"
-pwd
-cd -- cdtest
-pwd
 echo
-cd $WORKDIR
-
-rm -rf cdtest
