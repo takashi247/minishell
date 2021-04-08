4fda1a5122c734380e36fee8d12146fd44069ec3
s-ike <shunt_i@yahoo.co.jp>
2021-04-08 22:56:41 +0900

    update #80 processing of the pwd function when it has -- argument

--- srcs/pwd.c
+++ srcs/pwd.c
@@ -22,14 +22,16 @@ int
 	char	option[3];
 
 	g_status = STATUS_SUCCESS;
-	if (ft_get_cmd_option(option, args[1]))
+	if (args[1] && !ft_strcmp(args[1], "--"))
+		args++;
+	else if (ft_get_cmd_option(option, args[1]))
 	{
 		g_status = STATUS_GENERAL_ERR;
 		ft_put_cmderror_with_arg("pwd", CMD_OPTION_ERR, option);
 		ft_put_cmderror_with_help("pwd", CMD_PWD_HELP);
+		return (KEEP_RUNNING);
 	}
-	else
-		ft_putendl_fd(g_pwd, STDOUT_FILENO);
+	ft_putendl_fd(g_pwd, STDOUT_FILENO);
 	return (KEEP_RUNNING);
 }
 
