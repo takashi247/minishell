90898c756fab9abd0c86dd005b18fea12d249953
takashi247 <takashi247@gmail.com>
2021-05-09 00:07:26 +0900

    fix #170 SEGV with commands only with redirection and file name

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -99,6 +99,8 @@ void
 
 	if (!c || !environ)
 		exit(STATUS_GENERAL_ERR);
+	if (!c->args)
+		exit(g_status);
 	argv = ft_convert_list(c->args);
 	if (!argv)
 		exit(STATUS_GENERAL_ERR);
@@ -192,45 +194,49 @@ int
 	int		res;
 	int		std_fds[3];
 
-	res = STOP;
+	res = KEEP_RUNNING;
 	ft_save_fds(std_fds);
 	if (ft_set_redirection(c->redirects) == FALSE)
-		return (res);
-	argv = ft_convert_list(c->args);
-	if (!argv)
-	{
-		g_status = 1;
 		return (STOP);
+	if (c->args)
+	{
+		argv = ft_convert_list(c->args);
+		if (!argv)
+		{
+			g_status = 1;
+			return (STOP);
+		}
+		if (!ft_strcmp(argv[0], "echo"))
+			res = ft_echo(argv);
+		else if (!ft_strcmp(argv[0], "cd"))
+			res = ft_cd(argv);
+		else if (!ft_strcmp(argv[0], "pwd"))
+			res = ft_pwd(argv);
+		else if (!ft_strcmp(argv[0], "export"))
+			res = ft_export(argv);
+		else if (!ft_strcmp(argv[0], "unset"))
+			res = ft_unset(argv);
+		else if (!ft_strcmp(argv[0], "env"))
+			res = ft_env(argv);
+		else if (!ft_strcmp(argv[0], "exit"))
+			res = ft_exit(argv);
+		ft_clear_argv(&argv);
+		ft_restore_fds(std_fds);
 	}
-	if (!ft_strcmp(argv[0], "echo"))
-		res = ft_echo(argv);
-	else if (!ft_strcmp(argv[0], "cd"))
-		res = ft_cd(argv);
-	else if (!ft_strcmp(argv[0], "pwd"))
-		res = ft_pwd(argv);
-	else if (!ft_strcmp(argv[0], "export"))
-		res = ft_export(argv);
-	else if (!ft_strcmp(argv[0], "unset"))
-		res = ft_unset(argv);
-	else if (!ft_strcmp(argv[0], "env"))
-		res = ft_env(argv);
-	else if (!ft_strcmp(argv[0], "exit"))
-		res = ft_exit(argv);
-	ft_clear_argv(&argv);
-	ft_restore_fds(std_fds);
 	return (res);
 }
 
 static t_bool
 	is_builtin(t_command *c)
 {
-	if (!ft_strcmp((char* )(c->args->content), "echo")
+	if (c->args
+		&& (!ft_strcmp((char* )(c->args->content), "echo")
 		|| !ft_strcmp((char* )(c->args->content), "cd")
 		|| !ft_strcmp((char* )(c->args->content), "pwd")
 		|| !ft_strcmp((char* )(c->args->content), "export")
 		|| !ft_strcmp((char* )(c->args->content), "unset")
 		|| !ft_strcmp((char* )(c->args->content), "env")
-		|| !ft_strcmp((char* )(c->args->content), "exit"))
+		|| !ft_strcmp((char* )(c->args->content), "exit")))
 		return (TRUE);
 	else
 		return (FALSE);
