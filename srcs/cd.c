282c19f96d74390854841fd578db5c0eb611e92b
takashi247 <takashi247@gmail.com>
2021-05-20 12:43:12 +0900

    Feature/cdpath (#205)
    * update #164 cd function to use CDPATH (before refactoring)
    
    * update #164 to comply with norm
    
    * add functions for auto leak check
    
    * modify .gitignore and Makefile for leak check
    
    * fix leaks related to redirection errors
    
    * fix #205 norm errros
    
    * fix #205 based on received comments
    
    * fix #205 to undo Makefile and remove minishell_leaks from git
    
    Co-authored-by: s-ike <shunt_i@yahoo.co.jp>

--- srcs/cd.c
+++ srcs/cd.c
@@ -1,4 +1,5 @@
 #include "minishell_sikeda.h"
+#include "minishell_tnishina.h"
 
 static t_bool
 	validate_args(const char *args)
@@ -14,68 +15,6 @@ static t_bool
 		ret = FALSE;
 	}
 	return (ret);
-}
-
-static t_bool
-	exec_cd(const char *path, const char *args)
-{
-	t_bool	ret;
-
-	ret = FALSE;
-	if (!path)
-		return (ret);
-	if (chdir(path) != 0)
-	{
-		if (args)
-			ft_put_cmderror_with_arg("cd", strerror(errno), (char *)args);
-		else
-			ft_put_cmderror_with_arg("cd", strerror(errno), (char *)path);
-	}
-	else
-		ret = TRUE;
-	return (ret);
-}
-
-static void
-	update_pwd(const char *input_path)
-{
-	char	*oldpwd;
-	char	*cwd;
-
-	oldpwd = g_pwd;
-	cwd = getcwd(NULL, 0);
-	if (cwd)
-	{
-		ft_free(&cwd);
-		g_pwd = ft_make_full_path(input_path);
-	}
-	else
-	{
-		ft_put_cderror_no_current(errno);
-		if (g_pwd[ft_strlen(g_pwd) - 1] == '/')
-			g_pwd = ft_strjoin(g_pwd, input_path);
-		else
-			g_pwd = ft_join_path(g_pwd, input_path);
-	}
-	ft_free(&oldpwd);
-}
-
-static t_bool
-	update_path_env(const char *input_path)
-{
-	if (ft_getenv("OLDPWD") && ft_setenv_sep("OLDPWD", g_pwd) == UTIL_ERROR)
-	{
-		ft_put_cmderror("cd", strerror(errno));
-		return (FALSE);
-	}
-	update_pwd(input_path);
-	if (!g_pwd
-		|| (ft_getenv("PWD") && ft_setenv_sep("PWD", g_pwd) == UTIL_ERROR))
-	{
-		ft_put_cmderror("cd", strerror(errno));
-		return (FALSE);
-	}
-	return (TRUE);
 }
 
 int
@@ -94,11 +33,11 @@ int
 		return (KEEP_RUNNING);
 	if (path && *path == '\0')
 		path = g_pwd;
-	if (exec_cd(path, *args) == TRUE)
+	path = ft_strdup(path);
+	if (!path)
 	{
-		if (update_path_env(path) == FALSE)
-			return (STOP);
-		g_status = STATUS_SUCCESS;
+		ft_put_cmderror("cd", strerror(errno));
+		return (STOP);
 	}
-	return (KEEP_RUNNING);
+	return (ft_get_cd_result(&path, *args));
 }
