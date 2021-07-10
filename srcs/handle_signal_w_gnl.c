d4396439d8442bf17f5a2d2f039d0c6fb8802209
takashi247 <takashi247@gmail.com>
2021-07-10 17:11:01 +0900

    modify #239 remaining exit status which were hardcoded

--- srcs/handle_signal_w_gnl.c
+++ srcs/handle_signal_w_gnl.c
@@ -7,7 +7,7 @@ static void
 	{
 		ft_putstr_fd("\b\b  \b\n", STDERR_FILENO);
 		ft_putstr_fd(PROMPT, STDERR_FILENO);
-		g_ms.status = 1;
+		g_ms.status = STATUS_GENERAL_ERR;
 	}
 	else if (signal == SIGQUIT)
 		ft_putstr_fd("\b\b  \b\b", STDERR_FILENO);
@@ -19,11 +19,11 @@ void
 	if (signal(SIGINT, handle_prior_signal) == SIG_ERR)
 	{
 		ft_putstr_fd(strerror(errno), STDERR_FILENO);
-		exit(1);
+		exit(STATUS_GENERAL_ERR);
 	}
 	if (signal(SIGQUIT, handle_prior_signal) == SIG_ERR)
 	{
 		ft_putstr_fd(strerror(errno), STDERR_FILENO);
-		exit(1);
+		exit(STATUS_GENERAL_ERR);
 	}
 }
